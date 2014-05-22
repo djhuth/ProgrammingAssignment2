4e3fb5b3eaab38563965a93eb98699a26e23caf4
djhuth <huth.dirk@gmail.com>
2014-05-22 22:24:04 +0200

    comments and function stubs

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2, week 3, R-programming: 
+##
+## 1. function makeCacheMatrix creates a matrix object
+## 2. function cacheSolve retrieves inverse of the matrix
+##    returned by makeCacheMatrix from cache if it exists
+##    or else computes the inverse and stores it in cache
 
-## Write a short comment describing this function
+
+## creates a matrix object that can cache its inverse
+## cf. sample function makeVector
+## uses "superassignment"-operator <<- 
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## creates and returns the inverse of above matrix
+## if doesn't already exist in cache
+## if it does exist in cache (and is unchanged), it 
+## retrieves it from cach
+## cf. sample function cachemean
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
